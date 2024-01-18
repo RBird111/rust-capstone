960bc0ff7b6b7e6076d02453aa703fb1a39d0284
Roosevelt Burden <rooseveltburden@gmail.com>
2024-01-18 10:45:30 -0700

    added new review models

--- database/src/models/review.rs
+++ database/src/models/review.rs
@@ -1,11 +1,12 @@
-use crate::models::business::Business;
-use crate::models::user::User;
+use super::business::Business;
+use super::image::Image;
+use super::user::User;
+
 use crate::schema::{businesses, images, reviews, users};
 
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
-
-use super::image::Image;
+use serde_json::Value;
 
 #[derive(
     Associations,
@@ -31,7 +32,7 @@ pub struct Review {
 }
 
 impl Review {
-    pub fn eager_load(&self, conn: &mut PgConnection) -> QueryResult<serde_json::Value> {
+    pub fn eager_load(&self, conn: &mut PgConnection) -> QueryResult<Value> {
         let business: Business = businesses::table.find(self.business_id).first(conn)?;
         let user: User = users::table.find(self.user_id).first(conn)?;
         let images: Vec<Image> = images::table
@@ -39,18 +40,34 @@ impl Review {
             .filter(images::review_id.eq(self.id))
             .load(conn)?;
 
-        let result = serde_json::json!({
-            "id": self.id,
-            "rating": self.rating,
-            "body": self.body,
-            "user_id": self.user_id,
-            "business_id": self.business_id,
-            "user": serde_json::to_value(user).unwrap(),
-            "business": serde_json::to_value(business).unwrap(),
-            "images": serde_json::to_value(images).unwrap()
-        });
+        let result = serde_json::to_value(ReviewFull {
+            review: self.clone(),
+            user,
+            business,
+            images,
+        })
+        .unwrap();
 
-        Ok(result)
+        Ok(serde_json::json!({
+            "review": result
+        }))
+    }
+
+    fn parse(&self, conn: &mut PgConnection) -> QueryResult<Value> {
+        let business: Business = businesses::table.find(self.business_id).first(conn)?;
+        let user: User = users::table.find(self.user_id).first(conn)?;
+        let images: Vec<Image> = images::table
+            .select(Image::as_select())
+            .filter(images::review_id.eq(self.id))
+            .load(conn)?;
+
+        Ok(serde_json::to_value(ReviewFull {
+            review: self.clone(),
+            user,
+            business,
+            images,
+        })
+        .unwrap())
     }
 }
 
@@ -61,4 +78,35 @@ pub struct ReviewForm {
     pub body: String,
     pub user_id: i32,
     pub business_id: i32,
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub struct ReviewFull {
+    #[serde(flatten)]
+    pub review: Review,
+    pub business: Business,
+    pub user: User,
+    pub images: Vec<Image>,
+}
+
+#[derive(Debug, Clone)]
+pub struct ReviewArray(Vec<Review>);
+
+impl ReviewArray {
+    pub fn new(reviews: Vec<Review>) -> Self {
+        Self(reviews)
+    }
+
+    pub fn eager_load(&self, conn: &mut PgConnection) -> QueryResult<Value> {
+        let result: Vec<Value> = self
+            .0
+            .clone()
+            .into_iter()
+            .filter_map(|r| r.parse(conn).ok())
+            .collect();
+
+        Ok(serde_json::json!({
+            "reviews": result
+        }))
+    }
 }
