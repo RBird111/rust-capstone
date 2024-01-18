8b77a9415a0fc5966f9e02c0bcae3a7691f0f60e
Roosevelt Burden <rooseveltburden@gmail.com>
2024-01-18 16:27:48 -0700

    cleaned up main server function

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,7 @@
 pub mod routes;
 
 use actix_files as fs;
-use actix_web::web;
-use actix_web::{middleware, App, HttpResponse, HttpServer, Responder};
+use actix_web::{dev, middleware, web, App, HttpResponse, HttpServer, Responder};
 use database::ConnectionPool;
 
 pub type DBPool = web::Data<ConnectionPool>;
@@ -11,6 +10,20 @@ pub type DBPool = web::Data<ConnectionPool>;
 async fn home() -> impl Responder {
     let index = include_str!("../../frontend/build/index.html");
     HttpResponse::Ok().body(index)
+}
+
+fn default() -> fs::Files {
+    fs::Files::new("/", "./frontend/build")
+        .index_file("index.html")
+        .default_handler(|req: dev::ServiceRequest| {
+            let (http_req, _payload) = req.into_parts();
+
+            async {
+                let response =
+                    fs::NamedFile::open("./frontend/build/index.html")?.into_response(&http_req);
+                Ok(dev::ServiceResponse::new(http_req, response))
+            }
+        })
 }
 
 #[actix_web::main]
@@ -34,7 +47,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(pool.clone()))
             .service(home)
             .service(routes::api_routes())
-            .service(fs::Files::new("/", "./frontend/build").show_files_listing())
+            .service(default())
     })
     .bind(("localhost", port))?
     .run()
