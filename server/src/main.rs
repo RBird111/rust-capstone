61d214fa0646ed47e7b7e7866bb24b3b8029a346
Roosevelt Burden <rooseveltburden@gmail.com>
2024-01-21 17:47:46 -0700

    removed unused routes

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,36 +3,12 @@ pub mod routes;
 use actix_files::{Files, NamedFile};
 use actix_session::config::{BrowserSession, CookieContentSecurity};
 use actix_session::storage::CookieSessionStore;
-use actix_session::{Session, SessionMiddleware};
+use actix_session::SessionMiddleware;
 use actix_web::cookie::{Key, SameSite};
-use actix_web::{dev, get, middleware, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{dev, middleware, web, App, HttpServer};
 use database::ConnectionPool;
 
 pub type DBPool = web::Data<ConnectionPool>;
-
-#[derive(Debug, serde::Deserialize)]
-struct CookieModel {
-    message: String,
-}
-
-#[get("get_session")]
-async fn get_session(session: Session) -> impl Responder {
-    match session.get::<String>("message") {
-        Ok(message_option) => match message_option {
-            Some(message) => HttpResponse::Ok().body(message),
-            None => HttpResponse::NotFound().body("Not set."),
-        },
-        Err(_) => HttpResponse::InternalServerError().body("Session error."),
-    }
-}
-
-#[post("set_session")]
-async fn set_session(session: Session, model: web::Json<CookieModel>) -> impl Responder {
-    match session.insert("message", model.message.clone()) {
-        Ok(_) => HttpResponse::Created().body("Created."),
-        Err(_) => HttpResponse::InternalServerError().body("Error."),
-    }
-}
 
 fn session_middleware() -> SessionMiddleware<CookieSessionStore> {
     SessionMiddleware::builder(CookieSessionStore::default(), Key::from(&[0; 64]))
@@ -86,8 +62,6 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::new("%r %s"))
             .wrap(session_middleware())
             .app_data(web::Data::new(pool.clone()))
-            .service(get_session)
-            .service(set_session)
             .service(routes::api_routes())
             .service(default())
     })
